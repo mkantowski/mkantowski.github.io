40413b4edcae93c5fc138a7ad960fe941fc02577
zbeerladen <mkantowski@gmail.com>
2023-08-24 20:15:11 +0200

    poprawka astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://mkantowski.github.io",
+  //base: "/mkantowski.github.io"
   integrations: [
     tailwind(),
     image({
